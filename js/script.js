c71067553867a363e6e8532738c8a167f6af8230
MyUsername <alberto.baglivi@gmail.com>
2022-10-18 12:16:54 +0200

    Add the project

---
+++ js/script.js
@@ -0,0 +1,96 @@
+
+const grid = document.querySelector(".box_grid"); 
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click" , CreateCards);
+
+
+
+
+// SECTION DEFINITION FUNCTION 
+
+/**
+ * Description the function add the class card_grid_click who have blue backgroud,
+ *  and message of the internal value of card. We use this function on The Create Cards function
+ * because we want to have this effect when we click the card 
+ * @returns {void}
+ */
+function ShowNumber(){
+    this.classList.add("card_grid_click")      
+}
+
+
+/**
+ * Description Method for Create the cards, link directly on the option of first section on the header. 
+ * 7The function can create 100 , 81 or 49 cards, 
+ * depend on the choice of the operator 
+ * @returns {void}
+ */
+function CreateCards(){
+
+     grid.innerHTML = ""; 
+
+     const sel = document.getElementById("select_cards");   
+     let i= 1;
+
+     const arrayCards = []
+     const arrayBomb = GenerateBombs(sel.value);
+     
+     while(i<=sel.value){
+
+     let card = document.createElement('div');
+
+     if(sel.value === "100"){
+         card.classList.add("card_grid");
+      }
+     if(sel.value === "81"){
+         card.classList.add("card_grid_9");
+      }
+     if(sel.value === "49"){
+         card.classList.add("card_grid_7");
+       }
+
+     card.innerHTML = i;
+     card.addEventListener('click' , ShowNumber);
+
+     grid.appendChild(card);
+     arrayCards.push(card.innerHTML);
+    i++;
+
+}
+
+}
+
+
+function GenerateBombs(max){
+
+  const arrayB = []; 
+  let i = 1; 
+
+  while(i <= 16){
+
+      let num = Math.floor((Math.random()*(max-1))+1)
+
+      if(!arrayB.includes(num)){
+    
+       arrayB.push(num); 
+       i++; 
+
+      }
+
+  }
+
+  return arrayB
+ 
+}
+
+
+
+
+
+
+
+
+
+
